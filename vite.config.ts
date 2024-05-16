a3af3ab98a1f25c543d5ff49d2fd663a5179e24b
mohammadnuridin <developer.mohammadnuridin@gmail.com>
2024-05-16 19:45:40 +0700

    #

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,12 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  resolve: {
+    alias: {
+      "apollo-upload-client": "apollo-upload-client/public/index.js",
+    },
+  },
+});
